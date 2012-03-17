465d248875618ab29da93890d2840940c23077e1
Masahiro Hayashi <mhayashi1120@gmail.com>
2012-03-17 20:55:04 +0900

    add BOM test

--- wgrep-test.el
+++ wgrep-test.el
@@ -47,7 +47,7 @@
   (should (equal "FOO2\nBAZ\n" (wgrep-test--contents "test-data.txt")))
   (delete-file "test-data.txt"))
 
-(ert-deftest wgrep-multibyte ()
+(ert-deftest wgrep-bom-with-multibyte ()
   :tags '(wgrep)
   (wgrep-test--file "test-data.txt" "あ\nい\nう\n" 'utf-8-with-signature)
   (wgrep-test--grep "grep -nH -e 'あ' -A 2 test-data.txt")
@@ -67,7 +67,25 @@
   (should (equal "へのへのも\nへじ\nう\n" (wgrep-test--contents "test-data.txt")))
   (delete-file "test-data.txt"))
 
+(ert-deftest wgrep-bom-with-unibyte ()
+  :tags '(wgrep)
+  (wgrep-test--file "test-data.txt" "a\nb\n" 'utf-8-with-signature)
+  (wgrep-test--grep "grep -nH -e 'a' -A 2 test-data.txt")
+  (wgrep-change-to-wgrep-mode)
+  (goto-char (point-min))
+  ;; BOM check is valid.
+  (should (re-search-forward "test-data\.txt:[0-9]+:.*\\(a\\)$" nil t))
+  (replace-match "ABCD" nil nil nil 1)
+  ;; apply to buffer
+  (wgrep-finish-edit)
+  ;; save to file
+  (wgrep-save-all-buffers)
+  ;; compare file contents is valid
+  (should (equal "ABCD\nb\n" (wgrep-test--contents "test-data.txt")))
+  (delete-file "test-data.txt"))
+
 ;; TODO 
 ;; * wgrep-toggle-readonly-area
 ;; * wgrep-abort-changes
 ;; * wgrep-exit
+;; * broken file contents (invalid coding system)

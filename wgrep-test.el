ee2b0c1cd51374ce080a2302acf540eaddb709c2
Masahiro Hayashi <mhayashi1120@gmail.com>
2012-03-17 19:57:16 +0900

    simple test

---
+++ wgrep-test.el
@@ -0,0 +1,46 @@
+(require 'ert)
+
+(defun wgrep-test--grep (command)
+  (let* ((buf (grep command))
+         (proc (get-buffer-process buf)))
+    (while (eq (process-status proc) 'run) 
+      (sit-for 0.1))
+    (switch-to-buffer buf)))
+
+(defun wgrep-test--contents (file &optional cs)
+  (let ((coding-system-for-read cs))
+    (with-temp-buffer
+      (insert-file-contents file)
+      (buffer-string))))
+
+(defun wgrep-test--file (file contents)
+  ;; cleanup for convinience
+  (let ((buf (get-file-buffer file)))
+    (kill-buffer buf))
+  (write-region contents nil file))
+
+(ert-deftest wgrep-normal ()
+  :tags '(wgrep)
+  (wgrep-test--file "test-data.txt" "HOGE\nFOO\nBAZ\n")
+  (wgrep-test--grep "grep -nH -e .* test-data.txt")
+  (wgrep-change-to-wgrep-mode)
+  (goto-char (point-min))
+  ;; header is readonly
+  (should (re-search-forward "^grep" nil t))
+  (should-error (insert "ERROR") :type 'text-read-only)
+  ;; search hit line
+  (should (re-search-forward "HOGE" nil t))
+  ;; delete 1st line
+  (wgrep-flush-current-line)
+  (should (re-search-forward "FOO" nil t))
+  ;; replace 2nd line
+  (replace-match "FOO2")
+  ;; apply to buffer
+  (wgrep-finish-edit)
+  ;; save to file
+  (wgrep-save-all-buffers)
+  ;; compare file contents is valid
+  (should (equal "FOO2\nBAZ\n" (wgrep-test--contents "test-data.txt")))
+  (delete-file "test-data.txt"))
+
+

3028e9b31427c528d9343d458abcb2222813410f
Masahiro Hayashi <mhayashi1120@gmail.com>
2014-10-17 08:56:14 +0900

    fix wrong regexp. Closed #23

--- wgrep.el
+++ wgrep.el
@@ -4,7 +4,7 @@
 ;; Keywords: grep edit extensions
 ;; URL: http://github.com/mhayashi1120/Emacs-wgrep/raw/master/wgrep.el
 ;; Emacs: GNU Emacs 22 or later
-;; Version: 2.1.9
+;; Version: 2.1.10
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
@@ -196,7 +196,7 @@ a file."
   ;; This regexp come from Emacs-25 grep.el
   ;; Capture subexp 2 is still exists for the backward compatibility.
   ;; But will be removed in future release.
-  "^\\(.*?[^/\n]\\):\\([ \t]*\\)\\([1-9][0-9]*\\)[ \t]*:")
+  "^\\(.*?[^/\n]\\)\\(:[ \t]*\\)\\([1-9][0-9]*\\)[ \t]*:")
 
 (defvar wgrep-line-file-regexp wgrep-default-line-header-regexp
   "Regexp that match to line header of grep result.

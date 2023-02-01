3faf2e429ee36f1c71cd637f0a550d31c5a0bf56
Masahiro Hayashi <mhayashi1120@gmail.com>
2023-02-01 18:41:42 +0900

    revision up

--- wgrep-ag.el
+++ wgrep-ag.el
@@ -7,7 +7,7 @@
 ;; Package-Requires: ((wgrep "2.3.2"))
 ;; URL: http://github.com/mhayashi1120/Emacs-wgrep/raw/master/wgrep-ag.el
 ;; Emacs: GNU Emacs 25 or later
-;; Version: 2.3.3
+;; Version: 2.3.4
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as

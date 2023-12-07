874b1736f463e5495aa993d862378e8140efa12c
Xiyue Deng <manphiz@gmail.com>
2023-12-06 16:58:10 -0800

    Fix test extra loading
    * Since Emacs 29, test names cannot be duplicated.  This also applies
    to ert-deftest that are loaded through `requires', which means
    requiring another test unit may cause the same test being loaded more
    than once and causing the same error.
    
    * This patch puts common test utility in a separate module so as to
    avoid including another test unit as a whole.
    
    * Added file header comments and renamed functions/macros according to
    module name as required by CI.
    
    * See also Debian bug: https://bugs.debian.org/1057559

---
+++ wgrep-test-helper.el
@@ -0,0 +1,59 @@
+;;; wgrep-test-helper.el --- A wgrep test helper -*- lexical-binding: t -*-
+
+;; URL: http://github.com/mhayashi1120/Emacs-wgrep/raw/master/wgrep-test-helper.el
+;; Package-Requires: ((emacs "25.1"))
+
+;;; Commentary:
+
+;; A set of helper functions for ERT tests.
+
+(require 'wgrep)
+
+(defun wgre-test-helper--wait (buf)
+  (let ((proc (get-buffer-process buf)))
+    (while (eq (process-status proc) 'run)
+      (sit-for 0.1))
+    (sleep-for 0.2)
+    (switch-to-buffer buf)))
+
+(defun wgre-test-helper--grep (command)
+  (let ((buf (grep command)))
+    (wgre-test-helper--wait buf)))
+
+(defun wgre-test-helper--get-contents (file &optional cs)
+  (let ((coding-system-for-read cs))
+    (with-temp-buffer
+      (insert-file-contents file)
+      (buffer-string))))
+
+(defun wgre-test-helper--prepare-file (file contents &optional cs)
+  ;; cleanup for convenience
+  (let ((buf (get-file-buffer file)))
+    (when (buffer-live-p buf)
+      (kill-buffer buf)))
+  (let ((coding-system-for-write cs))
+    (write-region contents nil file)))
+
+(defun wgre-test-helper--cleanup-file (file)
+  (when (file-exists-p file)
+    (delete-file file))
+  (when (file-exists-p (concat file "~"))
+    (delete-file (concat file "~"))))
+
+(defmacro wgre-test-helper--default (&rest body)
+  `(let ((wgrep-change-readonly-file nil)
+         (wgrep-auto-save-buffer nil))
+     (progn ,@body)))
+
+(defun wgrep-test-helper--ag (string file)
+  (let ((buf (ag/search string default-directory :file-regex (regexp-quote file) :regexp t)))
+    (wgrep-test-helper--wait buf)))
+
+(defun wgrep-test-helper--deadgrep (string)
+  (let ((deadgrep-project-root-function (lambda () default-directory))
+        (deadgrep--search-type 'regexp))
+    (deadgrep string))
+  (wgrep-test-helper--wait (current-buffer)))
+
+
+(provide 'wgre-test-helper)
